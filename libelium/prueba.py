52e4a19d2a7a7b3223d9a193dd734d5859b2179b
Andres <andres.jaag@hotmail.es>
2022-09-08 09:44:24 -0500

    libeliumTemperature.v1

--- libelium/prueba.py
+++ libelium/prueba.py
@@ -1,13 +1,24 @@
 import serial
 import time
+from datetime import date
+from datetime import datetime
 
-archivo =open('E:\\classs\\C_C++\\holaq.txt','w')
+archivo =open('E:\\classs\\C_C++\\temperatura.txt','w')
 serialArduino=serial.Serial("COM7",115200)
 time.sleep(1)
+contador=0
 
 while True:
-    cad=serialArduino.readline().decode('ascii')
-    archivo.write(cad)
-    print(cad)
+    if contador == 0:
+        archivo.write("Captura;Fecha;Hora;Temperatura (Celsius)\n")
+        cad=serialArduino.readline().decode('ascii')
+    else:
+        print(contador)
+        now = datetime.now()
+        cad=serialArduino.readline().decode('ascii')
+        archivo.write(str(contador)+";"+str(now.year)+"/"+str(now.month)+"/"+str(now.day)
+        +";"+str(now.hour)+":"+str(now.minute)+":"+str(now.second)+";"+cad)
+        print(cad)
+    contador=contador+1
 
     
